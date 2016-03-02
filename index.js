9274f74152a947da3c4e6cd380e99d89d89dd753
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-03-02 16:34:35 +0100

    update index

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 var split = require('split');
 
 function filt(fn) {
-    process.stdin.pipe(split()).on('data', fn);
+    process.stdin.pipe(split()).on('data', function (line) {
+        var r = fn(line);
+        if (r != undefined) console.log(r);
+    });
 }
 
 module.exports = filt;
